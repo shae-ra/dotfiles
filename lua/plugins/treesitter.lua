8a2a5f457ee5a5f9d8dec415d675b4fe89ac9335
Juan José Ramayo <juan.jose.ramayo@gmail.com>
2024-06-17 18:32:02 -0300

    ADDED Treesitter config - rust

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "rust",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    }
+  }
+}
