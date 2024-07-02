e2b800e4664a8401b02821e985219996eb38a79b
Juan José Ramayo <juan.jose.ramayo@gmail.com>
2024-07-01 23:47:43 -0300

    ADDED harpoon & vim options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,54 @@
 -- use `vim.keymap.set` instead
 local map = vim.keymap.set
 
-map("n", "<leader>ho", function()
-  print("hola!")
-end)
-
+-- Tmux Navigate
 map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "tmux navigate left" })
 map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "tmux navigate right" })
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "tmux navigate down" })
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "tmux navigate up" })
+
+-- Custom ones
+map("n", "Q", "<nop>")
+map("n", "<leader>ra", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- zz
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- harpoon
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+map("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+map("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+map("n", "<leader>1", function()
+  harpoon:list():select(1)
+end)
+map("n", "<leader>2", function()
+  harpoon:list():select(2)
+end)
+map("n", "<leader>3", function()
+  harpoon:list():select(3)
+end)
+map("n", "<leader>4", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+map("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+map("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,35 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+local opt = vim.opt
+
+opt.nu = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.swapfile = false
+opt.backup = false
+--opt.undodir = os.getenv("HOME") .. "/./undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+
+opt.updatetime = 50
+
+opt.colorcolumn = "120"
